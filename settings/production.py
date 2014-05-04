401fae0cd99a6d15c7e7fc6f89b5b7107c4d0163
ckcollab <eric@ckcollab.com>
2014-05-04 13:04:25 -0700

    adds more logging

--- settings/production.py
+++ settings/production.py
@@ -1,5 +1,15 @@
 from .base import *
 
+
+
 import logging
 logger = logging.getLogger('waitress')
 logger.setLevel(logging.INFO)
+
+
+logging.basicConfig(
+    stream=sys.stdout,
+    level=logging.DEBUG,
+    format='"%(asctime)s %(levelname)8s %(name)s - %(message)s"',
+    datefmt='%H:%M:%S'
+)
