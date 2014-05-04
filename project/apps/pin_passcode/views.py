1728ee26fc50efd05c9a93c3404795d3c78dfd16
ckcollab <eric@ckcollab.com>
2014-05-04 01:49:49 -0700

    adds nice jquery passcode form

--- project/apps/pin_passcode/views.py
+++ project/apps/pin_passcode/views.py
@@ -16,16 +16,7 @@ def auth(request):
             user = get_user_model().objects.get(username=settings.PIN_PASSCODE_USERNAME)
             user.backend = 'django.contrib.auth.backends.ModelBackend'
 
-            #if not hasattr(request.user, 'session'):
-            #    engine = import_module(settings.SESSION_ENGINE)
-            #    session_key = request.COOKIES.get(settings.SESSION_COOKIE_NAME, None)
-            #    request.META = {}
-            #    request.user.session = engine.SessionStore(session_key)
-
-            #user = authenticate(username=settings.PIN_PASSCODE_USERNAME, password='eric')
             login(request, user)
-            return HttpResponseRedirect(request.GET.get('next', '/'))
-    # if request pin is right, login as settings.PIN_PASSCODE_USERNAME
-    #print request
+            return HttpResponse(status=200)
 
     return HttpResponse(status=401)
