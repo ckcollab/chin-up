450aee8b420ca3e1431700156a4fa6cb5327c84d
Eric Carmichael <eric@ckcollab.com>
2014-05-05 23:33:35 -0700

    using temp files instead

--- polished_backend.py
+++ polished_backend.py
@@ -1,21 +1,25 @@
 import subprocess
+import tempfile
 
 from polished.backends import DjangoBackend
 
 
 class ChinupDjangoBackend(DjangoBackend):
-    SCRIPT = ''
+    SCRIPT_PATH = ''
 
     def __init__(self, *args, **kwargs):
         super(ChinupDjangoBackend, self).__init__(*args, **kwargs)
-        self.SCRIPT = open("generate_data.py", "r").read()
+        script_data = open("generate_data.py", "r").read()
 
-        print 'WILL EXECUTE %s' % self.SCRIPT
+        f = tempfile.NamedTemporaryFile()
+        f.write(script_data)
+
+        self.SCRIPT = f.name
 
     def prepare_page(self, *args, **kwargs):
         super(ChinupDjangoBackend, self).prepare_page(*args, **kwargs)
 
         print 'PREPARING PAGE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
 
-        #subprocess.call(["python", "manage.py", "shell", "<", self.SCRIPT])
-        subprocess.check_call('python manage.py shell < %s' % self.SCRIPT, shell=True)
+        subprocess.call(["python", "manage.py", "shell", "<", self.SCRIPT])
+        #subprocess.check_call('python manage.py shell < %s' % self.SCRIPT, shell=True)
